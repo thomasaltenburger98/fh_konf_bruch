e7ae272059cfe400e3f4937dd7bf70658135866d
Thomas Altenburger <thomas.altenburger@hotmail.com>
2021-11-08 00:34:36 +0100

    implementation of both functions multiplicate(...)

--- src/Bruch.java
+++ src/Bruch.java
@@ -24,21 +24,12 @@ public class Bruch {
     }
 
     public Bruch multiplicate(Bruch b2) {
-        /* TODO:
-        *  Erstellen Sie für das Ergebnis eine neue Bruch-Variable
-        *   => Übergeben Sie im Konstruktur für numerator den
-        *       eigenen numerator multipliziert mit b2.getNumerator()
-        *   => Selbige auch für denominator
-        *  Retournieren Sie ihre neue Bruch-Variable
-        * */
-
-
-        return null;
+        Bruch result = new Bruch(this.numerator*b2.getNumerator(), this.denominator*b2.getDenominator());
+        return result;
     }
 
     public Bruch multiplicate(Bruch b2,  Bruch b3) {
-        // TODO: Multiplizieren Sie den eigenen Bruch mit b2 und b3
-
-        return null;
+        Bruch result = new Bruch(this.numerator*b2.getNumerator()*b3.getNumerator(), this.denominator*b2.getDenominator()*b3.getDenominator());
+        return result;
     }
 }
